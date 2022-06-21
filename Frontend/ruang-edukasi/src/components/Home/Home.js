f7f98f911c02c5b509140951e4e6abbac9b025c7
azizahcahya <zellchan07@gmail.com>
2022-06-21 21:03:10 +0700

    feat Home

---
+++ Frontend/ruang-edukasi/src/components/Home/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./Header";
+import Content from "./Content";
+import Footer from "../LandingPage/Footer";
+
+const Home = () => {
+    return (
+        <>
+        <p>hello</p>
+            <Header />
+            <Content />
+            <Footer />
+        </>
+    );
+}
+
+export default Home;
