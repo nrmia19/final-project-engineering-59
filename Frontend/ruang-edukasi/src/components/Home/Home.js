cd0951a8e7b07fa022955c8af94b85f21d396832
azizahcahya <zellchan07@gmail.com>
2022-06-21 21:30:38 +0700

    added home

--- Frontend/ruang-edukasi/src/components/Home/Home.js
+++ Frontend/ruang-edukasi/src/components/Home/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
-import Header from "..LandingPage/Header";
+import Header from "../LandingPage/Header";
 import Content from "./Content";
-import Footer from "..LandingPage/Footer";
+import Footer from "../LandingPage/Footer";
 
 const Home = () => {
     return (
