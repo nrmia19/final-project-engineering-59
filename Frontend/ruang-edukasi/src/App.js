3f9a07ce5d0922674408396189bf99ac3030d693
nrmia19 <miaanr20@gmail.com>
2022-06-21 21:19:00 +0700

    chore: update route in artikel

--- Frontend/ruang-edukasi/src/App.js
+++ Frontend/ruang-edukasi/src/App.js
@@ -4,18 +4,16 @@ import { Routes, Route } from 'react-router-dom';
 import Login from './components/Login/Login';
 import Register from './components/Register/Register';
 import LandingPage from './components/LandingPage/LandingPage';
+// import ArtikelPage from './components/ArtikelPage/ArtikelPage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
         <Routes>
           <Route path="/" element={<LandingPage />} />
-        </Routes>
-      </header>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          {/* <Route path="/artikel" element={<ArtikelPage />} /> */}
       </Routes>
     </div>
   );
