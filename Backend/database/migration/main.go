ba62d4488dc983c2ab203340efd68691454da264
Uswatun Hasanah <uswatunhaanah28@gmail.com>
2022-06-22 12:12:24 +0700

    complete unittest

--- Backend/database/migration/main.go
+++ Backend/database/migration/main.go
@@ -14,9 +14,26 @@ func main() {
 	}
 
 	_, err = db.Exec(`
-INSERT INTO articles(title, subject) VALUES
+	CREATE TABLE IF NOT EXISTS users (
+		id integer not null primary key AUTOINCREMENT,
+		username varchar(255) not null,
+		email varchar(255) not null,
+		password varchar(255) not null,
+		role varchar(255) not null,
+		loggedin boolean not null
+	);
+	CREATE TABLE IF NOT EXISTS articles (
+		id integer not null primary key AUTOINCREMENT,
+		title varchar(255) not null,
+		subject varchar(255) not null
+	);
+	INSERT INTO users(username, password, role, loggedin) VALUES
+    ('aditira', 'aditira@gmail.com','1234', 'admin', false),
+    ('dina', 'dina@gmail.com','4321', 'employee', false),
+    ('dito', 'dito@gmail.com','2552', 'employee', false);
+	
+	INSERT INTO articles(title, subject) VALUES
     ('tips menghafal cepat','jangan sambil main handphone');`)
-
 	if err != nil {
 		panic(err)
 	}
