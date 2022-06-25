8a41856de4e490c513b23003f90ba1fa420d64e5
azizahcahya <zellchan07@gmail.com>
2022-06-25 17:31:23 +0700

    feat: konsultasi page

--- Frontend/ruang-edukasi/src/App.js
+++ Frontend/ruang-edukasi/src/App.js
@@ -5,20 +5,24 @@ import Login from './components/Login/Login';
 import Register from './components/Register/Register';
 import LandingPage from './components/LandingPage/LandingPage';
 import Home from './components/Home/Home';
+import Konsultasi from './components/Konsultasi/Konsultasi';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
         <Routes>
           <Route path="/" element={<LandingPage />} />
         </Routes>
-      </header>
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/home" element={<Home />} />
+        
       </Routes>
+      <Routes>
+      <Route path="/konsultasi" element={<Konsultasi />} />
+        </Routes>
+
     </div>
   );
 }
