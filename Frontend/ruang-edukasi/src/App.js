19ad22d5c7291e3ce215705a0108c9da5c3b070a
nrmia19 <miaanr20@gmail.com>
2022-06-26 10:30:21 +0700

    chore: new styling

--- Frontend/ruang-edukasi/src/App.js
+++ Frontend/ruang-edukasi/src/App.js
@@ -4,8 +4,8 @@ import { Routes, Route } from 'react-router-dom';
 import Login from './components/Login/Login';
 import Register from './components/Register/Register';
 import LandingPage from './components/LandingPage/LandingPage';
-import Home from './components/Home/Home';
-import Konsultasi from './components/Konsultasi/Konsultasi';
+// import Home from './components/Home/Home';
+// import Konsultasi from './components/Konsultasi/Konsultasi';
 
 function App() {
   return (
@@ -14,7 +14,7 @@ function App() {
           <Route exact path="/" element={<LandingPage />} />
           <Route exact path="/login" element={<Login />} />
           <Route exact path="/register" element={<Register />} />
-          <Route path="/home" element={<Home />} />
+          {/* <Route path="/profile" element={<Home />} /> */}
       </Routes>
 
     </div>
