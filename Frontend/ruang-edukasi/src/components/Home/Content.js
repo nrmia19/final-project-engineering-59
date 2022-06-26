f77e9a2775e7960056026163066c03ca894c2ff5
nrmia19 <miaanr20@gmail.com>
2022-06-26 09:43:27 +0700

    chore: new styling content and header

--- Frontend/ruang-edukasi/src/components/Home/Content.js
+++ Frontend/ruang-edukasi/src/components/Home/Content.js
@@ -5,13 +5,14 @@ import img2 from '../../assets/images/lifetime.png';
 import img3 from '../../assets/images/konsultasi.png';
 import img4 from '../../assets/images/komunitas.png';
 import img5 from '../../assets/images/berbagi.png';
-import article1 from '../../assets/images/article-1.png';
-import article2 from '../../assets/images/article-2.png';
+import artikel from '../../assets/images/article-1.png';
+import konsul from '../../assets/images/article-2.png';
 import './content.scss';
 
 const Content = () => {
     return (
         <>
+            <main>
             <div className="content">
                 <div className="content-wrapper">
                 <Container>
@@ -82,44 +83,41 @@ const Content = () => {
                             />
                         </Col>
                     </Row> <br/>
-                    <p className='opening'>Apa yang menarik dari Ruang Edukasi??</p>
-                    <div className='article'>
-                        <img
-                            alt=""
-                            src={article1}
-                            left="12.57%"
-                            right="84.1%"
-                            top= "9.66%"
-                            bottom="86.51%" 
-                            width="262px"  
-                            height="202px" 
-                            className='article-img'    
-                        />
-                        <div className='detail'>
-                            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat</p>
-                            <button>Lihat Selengkapnya</button>
-                        </div>
-                    </div>
-                    <div className='article'>
-                        <img
-                            alt=""
-                            src={article2}
-                            left="12.57%"
-                            right="84.1%"
-                            top= "9.66%"
-                            bottom="86.51%" 
-                            width="262px"  
-                            height="202px" 
-                            className='article-img'    
-                        />
-                        <div className='detail'>
-                            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat</p>
-                            <button>Lihat Selengkapnya</button>
-                        </div>
-                    </div>
+                    <h4>Apa yang menarik dari Ruang Edukasi??</h4> <br/>
                 </Container>
                 </div>
-            </div>
+                <div className="content-title">
+                    <Container>
+                        <Row className="row g-3">
+                            <Col className="col-md-4">
+                                <img alt="article" src={artikel} width="300" height="200" />
+                            </Col>
+                            <Col className="col-md-6">
+                                    Tulis Artikel dan bagikan informasi yang kalian inginkan. Ini merupakan salah satu fitur yang dapat membantu kalian untuk meningkatkan kualitas pendidikan. Jangan ragu untuk mengedukasi antar siswa untuk bisa termotivasi belajar. Karena itu, Ruang Edukasi menyediakan fitur artikel.
+                                    <br />
+                                    <button className="button-selengkapnya" type="submit">
+                                       Lihat Selengkapnya
+                                    </button>
+                            </Col>
+                        </Row>
+                        <br />
+                        <Row className="row g-3">
+                            <Col className="col-md-4">
+                                <img alt="konsul" src={konsul} width="300" height="200"  />
+                            </Col>
+                            <Col className="col-md-6">
+                                    Konsultasi dengan guru atau teman anda. Ini merupakan salah satu fitur yang dapat membantu kita meningkatkan kualitas pendidikan. Anda bisa mengkonsultasikan perasaan atau psikologis kalian terkait dengan kegiatan belajar. Karena itu, Ruang Edukasi menyediakan fitur konsultasi.
+                                    <br />
+                                    <button className="button-selengkapnya" type="submit">
+                                       Lihat Selengkapnya
+                                    </button>
+                            </Col>
+                        </Row>
+                        
+                    </Container>
+                </div>
+                </div>
+            </main>
         </>
     );
 }
