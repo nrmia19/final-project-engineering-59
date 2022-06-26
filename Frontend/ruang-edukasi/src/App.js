da887fc5b81ee2905d8813b73170063c5a76d712
nrmia19 <miaanr20@gmail.com>
2022-06-26 12:44:13 +0700

    chore: delete file landing-page

--- Frontend/ruang-edukasi/src/App.js
+++ Frontend/ruang-edukasi/src/App.js
@@ -1,19 +1,19 @@
 // import './App.css';
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Login from './components/Login/Login';
-import Register from './components/Register/Register';
-import LandingPage from './components/LandingPage/LandingPage';
+// import Login from './components/Login/Login';
+// import Register from './components/Register/Register';
+// import LandingPage from './components/LandingPage/LandingPage';
 import Home from './components/Home/Home';
 
 function App() {
   return (
     <div className="App">
         <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/home" element={<Home />} />
+          {/* <Route path="/" element={<LandingPage />} /> */}
+          {/* <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} /> */}
+          <Route path="/home" element={<Home />} />
       </Routes>
     </div>
   );
