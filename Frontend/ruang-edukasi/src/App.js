0023ee0c716009be7c9b96b76844dd053669eeed
nrmia19 <miaanr20@gmail.com>
2022-06-26 20:37:36 +0700

    chore: complete

--- Frontend/ruang-edukasi/src/App.js
+++ Frontend/ruang-edukasi/src/App.js
@@ -4,7 +4,9 @@ import { Routes, Route } from 'react-router-dom';
 import Login from './components/Login/Login';
 import Register from './components/Register/Register';
 import LandingPage from './components/LandingPage/LandingPage';
-// import ArtikelPage from './components/ArtikelPage/ArtikelPage';
+import ArtikelPage from './components/ArtikelPage/ArtikelPage';
+import Home from './components/Home/Home';
+import Konsultasi from './components/Konsultasi/Konsultasi';
 
 function App() {
   return (
@@ -13,9 +15,9 @@ function App() {
           <Route exact path="/" element={<LandingPage />} />
           <Route exact path="/login" element={<Login />} />
           <Route exact path="/register" element={<Register />} />
-          {/* <Route exact path="/home" element={<Home />} />
+          <Route exact path="/home" element={<Home />} />
           <Route exact path="/artikel" element={<ArtikelPage />} />
-          <Route exact path="/konsultasi" element={<Konsultasi />} /> */}
+          <Route exact path="/konsultasi" element={<Konsultasi />} />
       </Routes>
 
     </div>
